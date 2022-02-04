f03497a51ff1be8ff09d039f75fb356196f5a20d
viniciustdaik <96190816+viniciustdaik@users.noreply.github.com>
2022-02-04 13:56:06 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,26 +6,29 @@ const Constraint = Matter.Constraint;
 var engine, world, backgroundImg;
 var canvas, angle, tower, ground, cannon;
 var cannonBall;
+var leftbutton, leftbuttonimg, rightbutton, rightbuttonimg;
 
 function preload() {
   backgroundImg = loadImage("./assets/background.gif");
   towerImage = loadImage("./assets/tower.png");
+  leftbuttonimg = loadImage("./assets/left_arrow.png");
+  rightbuttonimg = loadImage("./assets/right_arrow.png");
 }
 
 function setup() {
-  canvas = createCanvas(1200, 600);
+  canvas = createCanvas(windowWidth, windowHeight);//1200, 600
   engine = Engine.create();
   world = engine.world;
   angleMode(DEGREES);
   angle = 15;
-
+  
   ground = Bodies.rectangle(0, height - 1, width * 2, 1, { isStatic: true });
   World.add(world, ground);
 
-  tower = Bodies.rectangle(160, 350, 160, 310, { isStatic: true });
+  tower = Bodies.rectangle(160, windowHeight-280, 160, 310, { isStatic: true });//160, 350
   World.add(world, tower);
 
-  cannon = new Cannon(180, 110, 130, 100, angle);
+  cannon = new Cannon(180, windowHeight-490, 130, 100, angle);//180, 110, 130, 100, angle
   cannonBall = new CannonBall(cannon.x, cannon.y);
 }
 
