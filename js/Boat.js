ec1b75daf67c3d41961de2524b3975ee9355cff3
viniciustdaik <96190816+viniciustdaik@users.noreply.github.com>
2022-02-11 12:15:50 -0300

    Add files via upload

---
+++ js/Boat.js
@@ -0,0 +1,23 @@
+class Boat {
+  constructor(x, y, width, height, boatPos) {
+    this.body = Bodies.rectangle(x, y, width, height);
+    this.width = width;
+    this.height = height;
+
+    this.image = loadImage("./assets/boat.png");
+    this.boatPosition = boatPos;
+    World.add(world, this.body);
+  }
+
+  display() {
+    var angle = this.body.angle;
+    var pos = this.body.position;
+
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, this.boatPosition, this.width, this.height);
+    pop();
+  }
+}
