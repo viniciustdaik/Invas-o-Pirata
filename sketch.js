0cf652891191e483e52a6b201bd07fcc21edaf51
viniciustdaik <96190816+viniciustdaik@users.noreply.github.com>
2022-02-11 12:16:49 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,9 +3,10 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
-var engine, world, backgroundImg;
+var engine, world, backgroundImg, boat;
 var canvas, angle, tower, ground, cannon;
-var cannonBall;
+var balls = [];
+var boats = [];
 var leftbutton, leftbuttonimg, 
 rightbutton, rightbuttonimg, 
 cannonballbutton, cannonballbuttonimg;
@@ -22,8 +23,8 @@ function setup() {
   canvas = createCanvas(windowWidth, windowHeight);//1200, 600
   engine = Engine.create();
   world = engine.world;
-  angleMode(DEGREES);
-  angle = 15;
+  angleMode(DEGREES)
+  angle = 15
   
   leftbutton = createSprite(width/2-85, 45, 15, 15);
   leftbutton.addImage("leftb", leftbuttonimg);
@@ -37,37 +38,88 @@ function setup() {
   
   ground = Bodies.rectangle(0, height - 1, width * 2, 1, { isStatic: true });
   World.add(world, ground);
-  
+
   tower = Bodies.rectangle(160, windowHeight-280, 160, 310, { isStatic: true });//160, 350
   World.add(world, tower);
-  
+
   cannon = new Cannon(180, windowHeight-490, 130, 100, angle);//180, 110, 130, 100, angle
-  cannonBall = new CannonBall(cannon.x, cannon.y);
+  //cannonBall = new CannonBall(cannon.x, cannon.y);
+  //boat = new Boat(width-79, height - 60, 170, 170,-80);
 }
 
 function draw() {
   background(189);
   image(backgroundImg, 0, 0, width, height);
-  
-  keyReleased();
 
   Engine.update(engine);
   
+  
   rect(ground.position.x, ground.position.y, width * 2, 1);
-  push();
+ 
+
+  push();  
   imageMode(CENTER);
-  image(towerImage, tower.position.x, tower.position.y, 160, 310);
+  image(towerImage,tower.position.x, tower.position.y, 160, 310);
   pop();
 
+
+  //Matter.Body.setVelocity(boat.body,{x:-0.9, y:0})
+  //boat.display()
+  
+
+  for (var i = 0; i < balls.length; i++) {
+    showCannonBalls(balls[i], i);
+  }
+  showBoats();
   cannon.display();
-  cannonBall.display();
 
   drawSprites();
 }
 
-function keyReleased(){
-  if(keyCode === DOWN_ARROW
-  ||mouseIsOver(cannonballbutton)){
-    cannonBall.shoot();
+function keyPressed() {
+  if (keyCode === DOWN_ARROW) {
+    var cannonBall = new CannonBall(cannon.x, cannon.y);
+    cannonBall.trajectory = [];
+    Matter.Body.setAngle(cannonBall.body, cannon.angle);
+    balls.push(cannonBall);
+  }
+}
+
+function showCannonBalls(ball, index) {
+  if (ball) {
+    ball.display();
+  }
+}
+
+
+
+function keyReleased() {
+  if (keyCode === DOWN_ARROW
+    ||mouseIsOver(cannonballbutton)) {
+    balls[balls.length - 1].shoot();
+  }
+}
+
+function showBoats(){
+  if(boats.length > 0){
+    if(
+      boats[boats.length-1] === undefined||
+      boats[boats.length-1].body.position.x < width-300
+    ){
+      var positions = [-40, -60, -70, -20];
+      var position = random(positions);
+      var boat = new Boat(width, height-100, 170, 170, position);
+      boats.push(boat);
+    }
+  
+  for(var i=0; i<boats.length; i++){
+    if(boats[i]){
+      Matter.Body.setVelocity(boats[i].body, {x: -0.9, y: 0});
+      boats[i].display();
+    }
+  }
+  }else{
+    var boat = new Boat(width, height-60, 170, 170, -60);
+    boats.push(boat);
   }
 }
