da344b74013e9aac96e9de083dea624f628b557f
viniciustdaik <96190816+viniciustdaik@users.noreply.github.com>
2022-02-16 12:56:18 -0300

    Update Boat.js

--- js/Boat.js
+++ js/Boat.js
@@ -1,14 +1,23 @@
 class Boat {
   constructor(x, y, width, height, boatPos) {
+    
     this.body = Bodies.rectangle(x, y, width, height);
     this.width = width;
     this.height = height;
-
+    
     this.image = loadImage("./assets/boat.png");
     this.boatPosition = boatPos;
     World.add(world, this.body);
   }
-
+  
+  remove(index){
+    setTimeout(() => {
+      Matter.World.remove(world, boats[index].body);
+      delete boats[index];
+    }, 2000);
+    
+  }
+  
   display() {
     var angle = this.body.angle;
     var pos = this.body.position;
