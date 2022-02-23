008e16f6635618d122e6f92d496f9dc3d6f1087f
viniciustdaik <96190816+viniciustdaik@users.noreply.github.com>
2022-02-23 12:06:23 -0300

    Update CannonBall.js

--- js/CannonBall.js
+++ js/CannonBall.js
@@ -4,42 +4,55 @@ class CannonBall {
       isStatic: true
     };
     this.r = 30;
+    this.speed = 0.05;
     this.body = Bodies.circle(x, y, this.r, options);
     this.image = loadImage("./assets/cannonball.png");
+    this.animation = [this.image];
     this.trajectory = [];
+    this.isSink = false;
     World.add(world, this.body);
   }
-  
-  remove(index){
-    Matter.Body.setVelocity(this.body, {x: 0, y: 0});
+
+  animate() {
+    this.speed += 0.05;
+  }
+
+  remove(index) {
+    this.isSink = true;
+    Matter.Body.setVelocity(this.body, { x: 0, y: 0 });
+
+    this.animation = waterSplashAnimation;
+    this.speed = 0.05;
+    this.r = 150;
     setTimeout(() => {
       Matter.World.remove(world, this.body);
       delete balls[index];
     }, 1000);
-    
   }
-  
+
   shoot() {
-     var newAngle = cannon.angle - 28;
+    var newAngle = cannon.angle - 28;
     newAngle = newAngle *(3.14/180)
     var velocity = p5.Vector.fromAngle(newAngle);
     velocity.mult(0.5);
     Matter.Body.setStatic(this.body, false);
     Matter.Body.setVelocity(this.body, {
-      x: velocity.x *(180/3.14), 
-      y: velocity.y * (180/3.14)});
+      x: velocity.x *(180/3.14), y: velocity.y * (180/3.14)});
   }
 
   display() {
     var angle = this.body.angle;
     var pos = this.body.position;
-    
+    var index = floor(this.speed % this.animation.length);
+
     push();
+    translate(pos.x, pos.y);
+    rotate(angle);
     imageMode(CENTER);
-    image(this.image, pos.x, pos.y, this.r, this.r);
+    image(this.animation[index], 0, 0, this.r, this.r);
     pop();
 
-    if (this.body.velocity.x > 0 && pos.x > 10) {
+    if (this.body.velocity.x > 0 && pos.x > 10 && !this.isSink) {
       var position = [pos.x, pos.y];
       this.trajectory.push(position);
     }
@@ -49,3 +62,4 @@ class CannonBall {
     }
   }
 }
+
