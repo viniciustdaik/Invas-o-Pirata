29d6d8c54330d0b197207027e7dd800d9ab7f222
viniciustdaik <96190816+viniciustdaik@users.noreply.github.com>
2022-03-12 13:33:12 -0300

    Update Boat.js

--- js/Boat.js
+++ js/Boat.js
@@ -17,15 +17,17 @@ class Boat {
   }
 
   remove(index) {
-    this.animation = brokenBoatAnimation;
-    this.speed = 0.05;
-    this.width = 300;
-    this.height = 300;
-    this.isBroken = true;
-    setTimeout(() => {
-      Matter.World.remove(world, boats[index].body);
-      boats.splice(index, 1);
-    }, 2000);
+    if(this.isBroken == false){
+      this.animation = brokenBoatAnimation;
+      this.speed = 0.05;
+      this.width = 300;
+      this.height = 300;
+      this.isBroken = true;
+      setTimeout(() => {
+        Matter.World.remove(world, boats[index].body);
+        boats.splice(index, 1);
+      }, 2000);
+    }
   }
 
   display() {
